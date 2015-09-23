0dd758f5335059220f28d154515cd5cc7d49b4fd
ChrisC2 <chrisjc790@gmail.com>
2015-09-23 14:27:34 -0700

    functional with bugs

--- app/app.js
+++ app/app.js
@@ -1,54 +1,105 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var myApp = angular.module('myApp', [
-  'ngRoute'
-])
-// myApp.config(['$routeProvider', function($routeProvider) {
-//   $routeProvider.otherwise({redirectTo: '/view1'});
-// }]);
+var myApp = angular.module('myApp', [])
 
-.controller("map", function ($scope, mapFactory){
-$scope.data = {};
-  mapFactory.postMap()
-    .then(function(data){
-            console.log(data)
-            console.log($scope.data, "incoming data");
-
-            $scope.data = data;
-        });
+.controller("MapsController", function ($scope, shareCoordinates){
+  $scope.lat;
+  $scope.lng;
 
   $scope.initMap = function () {
-      var map = new google.maps.Map(document.getElementById('map'), {
-         center: {lat: -34.397, lng: 150.644},
-         zoom: 6
+    var el = document.getElementById("map");
+    //Create new map view
+    var map = new google.maps.Map(el, {
+         center: {lat: 34.050, lng: -118.250},
+         zoom: 15
       });
-    }
+      console.log("MAP CREATED :", map)
+      //creates window
+      var infoWindow = new google.maps.InfoWindow({map: map})
+       //HTML5 geolocation:
+       if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(function(position) {
+           var pos = {
+             lat: position.coords.latitude,
+             lng: position.coords.longitude
+           };
+           //Store coordinates in service for access;
+           shareCoordinates.setLat(position.coords.latitude);
+           shareCoordinates.setLng(position.coords.longitude);
+           console.log("lat: ", shareCoordinates.getLat());
+           console.log("lng :", shareCoordinates.getLng())
+           infoWindow.setPosition(pos); //takes object of coordinates
+           infoWindow.setContent('You are here');
+           map.setCenter(pos);
 
-    $scope.initMap();
+         }, function() {
+           handleLocationError(true, infoWindow, map.getCenter());
+         });
+       } else {
+         handleLocationError(false, infoWindow, map.getCenter());
+       }
 
-// $scope.postMap($scope.data);
-// $scope.getMap($scope.data);
-// console.log("postMAP: ", $scope.postMap($scope.data));
-// console.log("getMAP: ", $scope.getMap($scope.data));
+   }
+
 
 });
 
-myApp.factory("mapFactory", function ($http) {
-// var getMap = function (map) {
-//   return $http({
-//     method: "GET",
-//     url: "https://maps.googleapis.com/maps/api/js",
-//     data: map
-//   }).then(function(res) {
-//     console.log(res.data);
-//     return res.data
-//   })
-// }
+
+myApp.controller("IgController", function ($scope, DataFactory, shareCoordinates ){
+
+$scope.photoStorage = [];
+$scope.getPhotos = function () {
+  var lat = shareCoordinates.getLat();
+  var lng = shareCoordinates.getLng();
+  console.log(lat, lng);
+  var results = DataFactory.getData()
+    .then(function (data) {
+      data.data.forEach(function (item) {
+        var urls = item.images.standard_resolution.url
+        $scope.photoStorage.push(urls);
+      })
+console.log("controller storage: ", $scope.photoStorage)
+});
+}
+});
+
+myApp.service("shareCoordinates", function () {
+  var lat;
+  var lng;
+  return {
+    getLat: function () {
+      return lat;
+    },
+    getLng: function () {
+      return lng
+    },
+    setLat: function(value) {
+      lat = value;
+            },
+    setLng: function(value) {
+      lng = value;
+            }
+  }
+
+})
+
+myApp.factory("DataFactory", function ($http, shareCoordinates) {
+var getData = function (search) {
+  return $http({
+    method: "GET",
+    url: "https://api.instagram.com/v1/media/search?lat=" + shareCoordinates.getLat() + "&lng="+ shareCoordinates.getLng() + "&distance=5000&access_token=22125417.d904cd4.44abd06ef59d43e5b0fc7e9b4f347ebb",
+    data: search
+  }).then(function(res) {
+    return res.data
+  })
+
+
+  }
   var postMap = function (map) {
     return $http({
       method: "POST",
-      url: "https://maps.googleapis.com/maps/api/js",
+      url: map,
       data: map
     })
     .then(function (resp) {
@@ -58,7 +109,7 @@ myApp.factory("mapFactory", function ($http) {
   }
 
   return {
-    postMap: postMap,
-    // getMap: getMap
+    // postMap: postMap,
+    getData: getData
   }
 })
